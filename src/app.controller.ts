b3a55bb70391fe97d185ae4c8c7dbd90e66dba98
Arad Bin <aradbin@gmail.com>
2024-06-23 02:42:47 +0600

    basic whatsapp client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { Response } from 'express';
+import * as QRCode from 'qrcode';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private qrCode: string;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('qrcode')
+  async generateQrCode(@Res() response: Response) {
+    if (!this.qrCode) {
+      return response.status(404).send('QR code not found');
+    }
+
+    response.setHeader('Content-Type', 'image/png');
+    QRCode.toFileStream(response, this.qrCode);
+  }
+
+  @OnEvent('qrcode.created')
+  handleQrcodeCreatedEvent(qrCode: string) {
+    this.qrCode = qrCode;
+  }
+
+  @OnEvent('message.received')
+  async handleReceivedMessage(msg: any) {
+    const response = await fetch('http://localhost:8080/whatsapp/client/webhook', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(msg),
+    })
+    const data = await response.json();
+    console.log(`API response: ${data}`);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Client, LocalAuth } from 'whatsapp-web.js';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private client: Client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+      headless: true,
+      args: ['--no-sandbox', '--disable-gpu'],
+    },
+    webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html' },
+  });
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(private eventEmitter: EventEmitter2) {}
+
+  onModuleInit() {
+    this.client.on('qr', (qr) => {
+      this.logger.log(
+        `QrCode: http://localhost:8081/qrcode`,
+      );
+      this.eventEmitter.emit('qrcode.created', qr);
+    });
+
+    this.client.on('ready', () => {
+      this.logger.log("You're connected successfully!");
+    });
+
+    this.client.on('message', (msg) => {
+      this.logger.verbose(`${msg.from}: ${msg.body}`);
+      this.eventEmitter.emit('message.received', msg);
+    });
+
+    this.client.initialize();
   }
 }
